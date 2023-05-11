0b18c8b4becec14aa3bffdd805740ac179da77a6
UpsidePotential <48447427+UpsidePotential@users.noreply.github.com>
2023-05-10 20:14:34 -0500

    Update market-dashboard.ts

--- src/commands/market_dashboard/market-dashboard.ts
+++ src/commands/market_dashboard/market-dashboard.ts
@@ -79,7 +79,7 @@ export const RelRotGraphCommand: ICommand = {
   command: async (message: Message, services: any) => {
   try
   {
-    const image = await got('http://market-dashboard.waffletrade.lol/rrg');
+    const image = await got('${process.env.MARKET_DASHBOARD_URI}/rrg');
 
     await message.channel
         .send(
