66d7e21fa7d7718f7ae228e021e8c59fd5635ca2
Poppingfresh <poppingfresh6699@gmail.com>
2023-05-10 21:12:52 -0400

    Update extendHours.ts (#60)
    Bug fix to allow:
      1) Users to enter multiple arguments without breaking the bot. (Command will take only first input)
      2) If the ticker doesn't exist, catch the error and send a message.

--- src/commands/ExtendHours/extendHours.ts
+++ src/commands/ExtendHours/extendHours.ts
@@ -9,29 +9,33 @@ export const ExtendHoursCommand: ICommand = {
   showInHelp: true,
   trigger: (msg: Message) => msg.content.startsWith('!ext'),
   command: async (message: Message) => {
-    const ticker = message.content.replace('!ext', '').trim();
+    const ticker = message.content.split(' ')[1];
     const price = await getSymbolInfo(ticker);
-	//const extendedHoursClose = price.exhours.close;
-	console.log(price.extendedHours)
-    message.channel.send({
-      embeds: [{
-        color: 3447003,
-        title: ticker.toUpperCase(),
-		description: 'Extended hours prices (for when FinViz stops tracking it)',
-        fields: [
-          { name: 'Price', value: price.extendedHours.close.toString(), inline: true },
-		  { name: '% Change', value: price.extendedHours.pctChg, inline: true },
-		  { name: 'Time', value: price.extendedHours.time, inline: true },
+    if (typeof price.extendedHours == 'undefined') {
+	    message.channel.send("Unable to find data for this ticker... \n \
+		       Stop trying to break me.");
+	  }
+    else {
+      message.channel.send({
+        embeds: [{
+          color: 3447003,
+          title: ticker.toUpperCase(),
+		  description: 'Extended hours prices (for when FinViz stops tracking it)',
+          fields: [
+            { name: 'Price', value: price.extendedHours.close.toString(), inline: true },
+		    { name: '% Change', value: price.extendedHours.pctChg, inline: true },
+		    { name: 'Time', value: price.extendedHours.time, inline: true },
+          ],
+        },
         ],
-      },
-      ],
-    }).catch((error) => {
+      }).catch((error) => {
 		   // Error 50035 corresponds to empty field being sent to channel
 		   if (error.code == 50035) {
 			    message.channel.send("Someone finally got off their ass and put in an error catch.\n\
 		      !info broke. Blank field returned. It'll get fixed soon.");
 		   }
 	  });
+	}
     return Promise.resolve();
   },
 };
