45fa3b9f53e6ab4a594ef390cfe7548f6bba6f3e
Poppingfresh <poppingfresh6699@gmail.com>
2023-11-26 19:39:45 -0500

    Add Manatee Command (#83)
    Just a new shitposting command that usually just posts spy and qqq charts.

--- src/commands/Fuck/fuck.ts
+++ src/commands/Fuck/fuck.ts
@@ -35,4 +35,30 @@ export const GapperCommand: ICommand = {
   },
 };
 
-
+export const ManateeCommand: ICommand = {
+  name: 'Manatee',
+  helpDescription: 'Manatee',
+  showInHelp: false,
+  trigger: (msg: Message) => (msg.content.toLocaleLowerCase() === '!manatee'),
+  command: async (message: Message) => {
+    let i_rand =  Math.floor(Math.random() * 100); 
+	let quant = Math.floor(Math.random() * 10);
+	let cost = Math.floor(Math.random() * (18000 - 15000) + 15000);
+    if (i_rand < 6) {
+		await message.reply("**MODS**, probably Manatee")
+	} else if (i_rand >= 6 && i_rand < 12) {
+		await message.reply("According to my logs, Manatee is the most persecuted poster on this Discord.")
+	} else if (i_rand >= 12 && i_rand < 18) {
+	    await message.reply("__***BANATEE***__")
+	} else if (i_rand >= 18 && i_rand < 24) {
+	    await message.reply("Manatee's currently long "+quant.toString()+" NQ contracts from "+cost.toString())
+	} else if (i_rand >= 24 && i_rand < 30) {
+	    await message.reply("Manatee's currently short "+quant.toString()+" NQ contracts from "+cost.toString())
+    } else if (i_rand >= 30 && i_rand < 65) {
+	    await message.reply("$spy")
+	} else {
+	    await message.reply("$qqq")
+	}
+    return Promise.resolve();
+  },
+};
