92f6ea1088101ea72ab3b34cdb395e477d0d3ae5
Poppingfresh <poppingfresh6699@gmail.com>
2024-01-15 15:49:57 -0500

    Disable repeats (#101)
    Disable repeats on shitpost commands

--- src/commands/Fuck/fuck.ts
+++ src/commands/Fuck/fuck.ts
@@ -4,6 +4,26 @@ import { TickerTracker } from '../../services/tickerTracker';
 import { getFinvizScreenWholeTable } from '../Screener/finviz-screener';
 import { beginMatrix } from './hurfbot';
 
+const ManateeCases = [-1, -1, -1, -1, -1];
+const SepCases = [-1, -1, -1, -1, -1, -1];
+const ZephCases = [-1, -1, -1, -1, -1, -1];
+const DangCases = [-1, -1, -1, -1];
+const MilkCases = [-1, -1, -1];
+const JoshCases = [-1, -1, -1];
+
+
+function weightedRandomCase(probabilities: number[]): number {
+  const totalWeight = probabilities.reduce((total, weight) => total + weight, 0);
+  let random = Math.floor(Math.random() * totalWeight);
+  for (let i = 0; i < probabilities.length; i++) {
+    if (random < probabilities[i]) {
+      return i;
+    }
+    random -= probabilities[i];
+  }
+  return probabilities.length - 1;
+}
+
 const DeleteMessage = (message: Message, messageId: string): void => {
   message.channel.messages.fetch(messageId).then((value) => {
     value.delete().catch(() => {});
@@ -43,21 +63,33 @@ export const ManateeCommand: ICommand = {
   showInHelp: false,
   trigger: (msg: Message) => (msg.content.toLocaleLowerCase() === '!manatee'),
   command: async (message: Message) => {
-    let i_rand =  Math.floor(Math.random() * 100); 
-	let quant = Math.floor(Math.random() * 10);
-	let cost = Math.floor(Math.random() * (18000 - 15000) + 15000);
-    if (i_rand < 20) {
-		await message.reply({ files : ["./src/commands/Fuck/images/Manatee1.png"] })
-	} else if (i_rand >= 20 && i_rand < 40) {
-		await message.reply("According to my logs, Manatee is the most persecuted poster on this Discord.")
-	} else if (i_rand >= 40 && i_rand < 60) {
-	    await message.reply("__***BANATEE***__")
-	} else if (i_rand >= 60 && i_rand < 80) {
-	    await message.reply({ files : ["./src/commands/Fuck/images/Manatee2.jpg"] })
-    } else if (i_rand >= 80 && i_rand < 90) {
-	    await message.reply("$spy")
-	} else {
-	    await message.reply("$qqq")
+    const probabilities = [20, 20, 20, 20, 10, 10];
+    let newCase;
+    do {
+      newCase = weightedRandomCase(probabilities);
+    } while (ManateeCases.includes(newCase));
+	ManateeCases.shift();
+	ManateeCases.push(newCase);
+	
+    switch (newCase) {
+		case 0:
+			await message.reply({ files : ["./src/commands/Fuck/images/Manatee1.png"] });
+			break;
+		case 1:
+			await message.reply("According to my logs, Manatee is the most persecuted poster on this Discord.");
+			break;
+		case 2:
+			await message.reply("__***BANATEE***__");
+			break;
+		case 3:
+			await message.reply({ files : ["./src/commands/Fuck/images/Manatee2.jpg"] });
+			break;
+		case 4:
+			await message.reply("$spy");
+			break;
+		default:
+			await message.reply("$qqq")
+			break;
 	}
     return Promise.resolve();
   },
@@ -93,30 +125,45 @@ export const SepistCommand: ICommand = {
   showInHelp: false,
   trigger: (msg: Message) => (msg.content.toLocaleLowerCase() === '!sepist'),
   command: async (message: Message) => {
-	let i_rand =  Math.floor(Math.random() * 100); 
-    if (i_rand < 15) {
-		await message.reply({ files : ["./src/commands/Fuck/images/Sep5.jpg"] })
-	} else if (i_rand >= 15 && i_rand < 25) {
-		await message.reply({ files : ["./src/commands/Fuck/images/Sepist3.png"] })
-	} else if (i_rand >= 25 && i_rand < 35) {
-		await message.reply({ files : ["./src/commands/Fuck/images/Sep2.png"] })
-	} else if (i_rand >= 35 && i_rand < 45) {
-		await message.reply('https://www.ziprecruiter.com/jobs-search?search=babysitter&location=Long+Island%2C+NY')
-	} else if (i_rand >= 45 && i_rand < 70) {
-		const fs = require('fs')
-		const fileContent = fs.readFileSync('./src/commands/Fuck/images/Sepist.txt', 'utf-8');
-		const lines = fileContent.split('\n');
-		const line = lines[Math.floor(Math.random() * lines.length)]
-		await message.reply(line)
-	} else if (i_rand >= 70 && i_rand < 90) {
-		await message.reply("$/bz d")
-	} else {
-	    await message.reply("I'm a humble landlord, carrying the weight,\n\Juggling repairs and an ever-growing slate.\n\
+	const probabilities = [15, 10, 10, 10, 25, 20, 10];
+    let newCase;
+    do {
+      newCase = weightedRandomCase(probabilities);
+    } while (SepCases.includes(newCase));
+	SepCases.shift();
+	SepCases.push(newCase);
+    
+	switch (newCase) {
+		case 0:
+			await message.reply({ files : ["./src/commands/Fuck/images/Sep5.jpg"] });
+			break;
+		case 1:
+			await message.reply({ files : ["./src/commands/Fuck/images/Sepist3.png"] });
+			break;
+		case 2:
+			await message.reply({ files : ["./src/commands/Fuck/images/Sep2.png"] });
+			break;
+		case 3:
+			await message.reply('https://www.ziprecruiter.com/jobs-search?search=babysitter&location=Long+Island%2C+NY');
+			break;
+		case 4:
+			const fs = require('fs')
+			const fileContent = fs.readFileSync('./src/commands/Fuck/images/Sepist.txt', 'utf-8');
+			const lines = fileContent.split('\n');
+			const line = lines[Math.floor(Math.random() * lines.length)]
+			await message.reply(line)
+			break;
+		case 5:
+			await message.reply("$/bz d")
+			break;
+		default:
+			await message.reply("I'm a humble landlord, carrying the weight,\n\Juggling repairs and an ever-growing slate.\n\
 Society will never understand my plight,\n\In this landlord life, it's a constant fight.\n\n\Repainting walls and rented halls,\n\
 Witness to tenants' rise and falls.\n\Leaving garbage with maggots in their wake,\n\A landlord's dilemma, decisions to make.\n\n\
 Every late-night call, every rent dispute,\n\A puzzle to solve, a neverending pursuit\n\Of keeping the peace in this rented space,\n\
 Being a landlord, it's no easy embrace.\n\n\So here's to the landlords, modern day knights!\n\Facing the challenges with all their might!\n\
 A thankless job, their stories untold,\n\Providing valuable services, while constantly trolled.")
+			break;
 	}
     return Promise.resolve();
   },
@@ -210,25 +257,40 @@ export const ZephCommand: ICommand = {
   showInHelp: false,
   trigger: (msg: Message) => (msg.content.toLocaleLowerCase() === '!zeph'),
   command: async (message: Message) => {
-	let i_rand =  Math.floor(Math.random() * 100); 
-    if (i_rand < 15) {
-		await message.reply({ files : ["./src/commands/Fuck/images/Zeph1.jpg"] })
-	} else if (i_rand >= 16 && i_rand < 30) {
-		await message.reply({ files : ["./src/commands/Fuck/images/Zeph2.png"] })
-	} else if (i_rand >= 30 && i_rand < 60) {
-		const fs = require('fs')
-		const fileContent = fs.readFileSync('./src/commands/Fuck/images/Cars.txt', 'utf-8');
-		const lines = fileContent.split('\n');
-		const line = lines[Math.floor(Math.random() * lines.length)]
-		await message.reply(line)
-	} else if (i_rand >= 60 && i_rand < 80) {
-		await message.reply({ files : ["./src/commands/Fuck/images/Zeph3.jpg"] })
-	} else if (i_rand >= 80 && i_rand < 87) {
-		await message.reply("$kold")
-	} else if (i_rand >= 87 && i_rand < 94) {
-		await message.reply("$sgov m")
-	} else {
-	    await message.reply("$svix d")
+	const probabilities = [20, 20, 20, 10, 10, 10, 10];
+    let newCase;
+    do {
+      newCase = weightedRandomCase(probabilities);
+    } while (ZephCases.includes(newCase));
+	ZephCases.shift();
+	ZephCases.push(newCase);
+	
+    switch (newCase) {
+		case 0:
+			await message.reply({ files : ["./src/commands/Fuck/images/Zeph1.jpg"] })
+			break;
+		case 1:
+			await message.reply({ files : ["./src/commands/Fuck/images/Zeph2.png"] })
+			break;
+		case 2:
+			const fs = require('fs')
+			const fileContent = fs.readFileSync('./src/commands/Fuck/images/Cars.txt', 'utf-8');
+			const lines = fileContent.split('\n');
+			const line = lines[Math.floor(Math.random() * lines.length)]
+			await message.reply(line)
+			break;
+		case 3:
+			await message.reply({ files : ["./src/commands/Fuck/images/Zeph3.jpg"] })
+			break;
+		case 4:
+			await message.reply("$kold")
+			break;
+		case 5:
+			await message.reply("$sgov m")
+			break;
+		default:
+			await message.reply("$svix d")
+			break;
 	}
     return Promise.resolve();
   },
@@ -240,35 +302,59 @@ export const MilkCommand: ICommand = {
   showInHelp: false,
   trigger: (msg: Message) => (msg.content.toLocaleLowerCase() === '!milk'),
   command: async (message: Message) => {
-	let i_rand =  Math.floor(Math.random() * 100); 
-    if (i_rand < 15) {
-		await message.reply({ files : ["./src/commands/Fuck/images/ohio.gif"] })
-	} else if (i_rand >= 15 && i_rand < 30) {
-		const fs = require('fs')
-		const fileContent = fs.readFileSync('./src/commands/Fuck/images/Bridges.txt', 'utf-8');
-		const lines = fileContent.split('\n');
-		const line = lines[Math.floor(Math.random() * lines.length)]
-		await message.reply(line)
-	} else if (i_rand >= 30 && i_rand < 45) {
-		await message.reply("Just lmao that the market is pumping. Of course it is. Gotta love that rational and efficient market!")
-	} else if (i_rand >= 45 && i_rand < 60) {
-	    await message.reply("I see the current S&P 500 as a colossal suspension bridge pushed to its limits. \
+	const probabilities = [10, 10, 15, 15, 25, 25];
+    let newCase;
+    do {
+      newCase = weightedRandomCase(probabilities);
+    } while (MilkCases.includes(newCase));
+	MilkCases.shift();
+	MilkCases.push(newCase);
+	
+     switch (newCase) {
+		case 0:
+			await message.reply({ files : ["./src/commands/Fuck/images/ohio.gif"] })
+			break;
+		case 1:
+			const fs = require('fs')
+			const fileContent = fs.readFileSync('./src/commands/Fuck/images/Bridges.txt', 'utf-8');
+			const lines = fileContent.split('\n');
+			const line = lines[Math.floor(Math.random() * lines.length)]
+			await message.reply(line)
+			break;
+		case 2:
+			await message.reply("Just lmao that the market is pumping. Of course it is. Gotta love that rational and efficient market!")
+			break;
+		case 3:
+			await message.reply("I see the current S&P 500 as a colossal suspension bridge pushed to its limits. \
 The market resembles a structure with cables drawn tightly, mirroring an elevated price-to-earnings ratio. The \
 economic landscape appears as turbulent winds, with inflation as the wind shear pushing the structure to its absolute limits. \
 I believe investors should navigate cautiously. These turbulent times are going to stress the metaphorical bridge and those slight vibrations of \
 uncertainty will most likely turn into a resonant vibration that will bring down the entire financial system. \ Consider a portfolio recalibration to ensure \
 you can stay afloat in the ever-changing financial landscape.")
-	} else {
-		let url = 'https://finviz.com/screener.ashx?v=110&s=ta_topgainers';
-		const table = await getFinvizScreenWholeTable(url);
-		const arrayLength = Math.min(table.length, 5);
-		const fields = table.slice(0, arrayLength).map((value) => ({
-			name: value.ticker,
-			value: `Price: ${value.price} Change: ${value.change}`,
-		}));
-		await message.reply("$"+fields[Math.floor(Math.random() * arrayLength)-1].name)
-	    await message.reply("did you listen???")
-	}
+			break;
+		case 4:
+			let url = 'https://finviz.com/screener.ashx?v=110&s=ta_topgainers';
+			const table = await getFinvizScreenWholeTable(url);
+			const arrayLength = Math.min(table.length, 5);
+			const fields = table.slice(0, arrayLength).map((value) => ({
+				name: value.ticker,
+				value: `Price: ${value.price} Change: ${value.change}`,
+			}));
+			await message.reply("$"+fields[Math.floor(Math.random() * arrayLength)-1].name)
+			await message.reply("did you listen???")
+			break;
+		default:
+			let url_fa = 'https://finviz.com/screener.ashx?v=111&f=cap_largeover,exch_nyse,fa_fpe_high,fa_pe_high';
+			const table1 = await getFinvizScreenWholeTable(url_fa);
+			const arrayLength1 = Math.min(table1.length, 5);
+			const fields1 = table1.slice(0, arrayLength1).map((value) => ({
+				name: value.ticker,
+				value: `Price: ${value.price} Change: ${value.change}`,
+			}));
+			await message.reply("!fa "+fields1[Math.floor(Math.random() * arrayLength1)-1].name)
+			await message.reply("Now here's a stock with a sane P/E to invest in...")
+			break;
+		}
     return Promise.resolve();
   },
 };
@@ -279,28 +365,42 @@ export const JoshCommand: ICommand = {
   showInHelp: false,
   trigger: (msg: Message) => (msg.content.toLocaleLowerCase() === '!josh'),
   command: async (message: Message) => {
-	let i_rand =  Math.floor(Math.random() * 100); 
-    if (i_rand < 15) {
-		await message.reply("To take a photo while *subtly* including your shorts or pants in the frame, \
+	const probabilities = [15, 10, 15, 25, 25, 10];
+    let newCase;
+    do {
+      newCase = weightedRandomCase(probabilities);
+    } while (JoshCases.includes(newCase));
+	JoshCases.shift();
+	JoshCases.push(newCase);
+    
+	switch (newCase) {
+		case 0:
+			await message.reply("To take a photo while *subtly* including your shorts or pants in the frame, \
 consider positioning the camera at a slight angle to capture both yourself and the lower half of your body.\n\
 Use a timer or a remote shutter to allow yourself some distance from the camera, ensuring a natural and composed pose. \
 Pay attention to the lighting to ***enhance*** the 'details' of your bulge.\n\
 Experiment with different angles and poses to find the most flattering composition for your *outline*. \
 Remember to maintain a confident and relaxed expression for a genuine and appealing photograph.")
-	} else if (i_rand >= 15 && i_rand < 30) {
-		await message.reply({ files : ["./src/commands/Fuck/images/Josh2.png"] })
-	} else if (i_rand >= 30 && i_rand < 45) {
-		await message.reply({ files : ["./src/commands/Fuck/images/Josh.png"] })
-	} else if (i_rand >= 45 && i_rand < 75) {
-		const fs = require('fs')
-		const fileContent = fs.readFileSync('./src/commands/Fuck/images/Josh.txt', 'utf-8');
-		const lines = fileContent.split('\n');
-		const line = lines[Math.floor(Math.random() * lines.length)]
-		await message.reply(line)
-	} else if (i_rand >= 75 && i_rand < 90) {
-		await message.reply({ files : ["./src/commands/Fuck/images/Josh4.png"] })
-	} else {
-	    await message.reply("$tqqq d")
+			break;
+		case 1:
+			await message.reply({ files : ["./src/commands/Fuck/images/Josh2.png"] })
+			break;
+		case 2:
+			await message.reply({ files : ["./src/commands/Fuck/images/Josh.png"] })
+			break;
+		case 3:
+			const fs = require('fs')
+			const fileContent = fs.readFileSync('./src/commands/Fuck/images/Josh.txt', 'utf-8');
+			const lines = fileContent.split('\n');
+			const line = lines[Math.floor(Math.random() * lines.length)]
+			await message.reply(line)
+			break;
+		case 4:
+			await message.reply({ files : ["./src/commands/Fuck/images/Josh4.png"] })
+			break;
+		default:
+			await message.reply("$tqqq d")
+			break;
 	}
     return Promise.resolve();
   },
@@ -460,21 +560,33 @@ export const DanglingCommand: ICommand = {
   showInHelp: false,
   trigger: (msg: Message) => (msg.content.toLocaleLowerCase() === '!dangling'),
   command: async (message: Message) => {
-	let i_rand =  Math.floor(Math.random() * 100); 
-    if (i_rand < 20) {
-		await message.reply({ files : ["./src/commands/Fuck/images/Dang1.png"] })
-	} else if (i_rand >= 20 && i_rand < 50 ) {
-		await message.reply({ files : ["./src/commands/Fuck/images/peeinpool.gif"] })
-	} else if (i_rand >= 50 && i_rand < 80 ) {
-		const fs = require('fs')
-		const fileContent = fs.readFileSync('./src/commands/Fuck/images/Dangling.txt', 'utf-8');
-		const lines = fileContent.split('\n');
-		const line = lines[Math.floor(Math.random() * lines.length)]
-		await message.reply(line)
-	} else if (i_rand < 80 && i_rand < 90) {
-		await message.reply("$dkng d")
-	} else {
-		await message.reply("$penn d")
+	const probabilities = [20, 30, 40, 5, 5];
+    let newCase;
+    do {
+      newCase = weightedRandomCase(probabilities);
+    } while (DangCases.includes(newCase));
+	DangCases.shift();
+	DangCases.push(newCase); 
+	
+    switch (newCase) {
+		case 0:
+			await message.reply({ files : ["./src/commands/Fuck/images/Dang1.png"] });
+			break;
+		case 1:
+			await message.reply({ files : ["./src/commands/Fuck/images/peeinpool.gif"] })
+			break;
+		case 2:
+			const fs = require('fs')
+			const fileContent = fs.readFileSync('./src/commands/Fuck/images/Dangling.txt', 'utf-8');
+			const lines = fileContent.split('\n');
+			const line = lines[Math.floor(Math.random() * lines.length)]
+			await message.reply(line)
+			break;
+		case 3:
+			await message.reply("$dkng d")
+			break;
+		default:
+			await message.reply("$penn d")
 	}
     return Promise.resolve();
   },
