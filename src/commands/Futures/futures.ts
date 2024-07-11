71b51001dda8f1eddb11af1fcb99d09330d3d317
Poppingfresh <poppingfresh6699@gmail.com>
2024-07-11 17:55:54 -0400

    RegressionTests
    Just change the answer, simpol

--- src/commands/Futures/futures.ts
+++ src/commands/Futures/futures.ts
@@ -69,7 +69,7 @@ export const FuturesCommand: ICommand = {
 		ticker = `@` + getTicker(ticker).slice(1);
 		const file = `https://charts-node.finviz.com/chart.ashx?cs=l&t=${ticker}&tf=${timePeriod}&s=linear&ct=candle_stick&o[0][ot]=sma&o[0][op]=20&o[0][oc]=FF8F33C6&o[1][ot]=sma&o[1][op]=50&o[1][oc]=DCB3326D&o[2][ot]=sma&o[2][op]=200&o[2][oc]=DC32B363&o[3][ot]=patterns&o[3][op]=&o[3][oc]=000`
 		//const file = `https://charts-node.finviz.com/chart.ashx?cs=m&t=@${ticker}&tf=${timePeriod}&s=linear&ct=candle_stick&f=1`+ `x=${Math.random()}.png`
-		TickerTracker.postTicker(ticker, message.author.id, 'future');
+		//TickerTracker.postTicker(ticker, message.author.id, 'future');
 		const image = await got(file);
 		const sentMessage = await message.channel.send(
 			{
@@ -78,8 +78,7 @@ export const FuturesCommand: ICommand = {
 			  ],
 			},
 		  );
-		TickerTracker.lastTicker(message.author.id, message.id, (sentMessage as Message).id);
-		console.log("At least we are in here...")
+		//TickerTracker.lastTicker(message.author.id, message.id, (sentMessage as Message).id);
 		return Promise.resolve();
 	} catch {
 		let ticker = message.content.toLowerCase().split(' ')[0].substring(2);
