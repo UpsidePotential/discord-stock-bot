b1b229cba9de0a2406fc157e51ea78cfd2b43047
DonEquiss <equiss@gmail.com>
2024-12-16 17:37:01 -0700

    update stock charts finviz url to fix indicators not loading, update params for indicators (#124)
    Co-authored-by: Hiba <Hibajubwa@gmail.com>

--- src/common.spec.ts
+++ src/common.spec.ts
@@ -11,10 +11,10 @@ describe('Common', () => {
     result: string;
   }
   const testData: TestData[] = [
-    { key: 'indicators', options: ['rsi'], result: ',rsi_b_14,sma_50,sma_200,sma_20' },
-    { key: 'indicators', options: [], result: '' },
-    { key: 'chart_type', options: ['line'], result: 'l' },
-    { key: 'chart_type', options: [], result: 'c' },
+    { key: 'indicators', options: ['vwap'], result: '&o[0][ot]=vwap&o[0][op]=0&o[0][oc]=7d3c98&o[1][ot]=patterns&o[1][op]=0&o[1][oc]=000' },
+    { key: 'indicators', options: [], result: '&o[0][ot]=sma&o[0][op]=20&o[0][oc]=FF8F33C6&o[1][ot]=sma&o[1][op]=50&o[1][oc]=DCB3326D&o[2][ot]=sma&o[2][op]=200&o[2][oc]=DC32B363&o[3][ot]=patterns&o[3][op]=&o[3][oc]=000' },
+    { key: 'chart_type', options: ['line'], result: 'line' },
+    { key: 'chart_type', options: [], result: 'candle_stick' },
     { key: 'time_period', options: ['m'], result: 'm' },
     { key: 'time_period', options: [], result: 'i5' },
     { key: 'time_period_futures', options: ['d'], result: 'd' },
